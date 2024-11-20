0a4a0029d97bc292d48ce20b96714691941c9abd
julianblanco7 <jorjuela@uniempresarial.edu.co>
2024-11-20 11:59:07 -0500

    ALGO

--- src/main/java/co/edu/EntidadFinanciera/infraestructura/PersonaRest.java
+++ src/main/java/co/edu/EntidadFinanciera/infraestructura/PersonaRest.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController; 
 
 
-/*holaaaa*/
+/*holaaaaHOLMAN*/
 
 @CrossOrigin(origins = "*", maxAge = 3600) 
 @RestController 
